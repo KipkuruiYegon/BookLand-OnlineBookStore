357fe732104cd4b8b7020a3a848b4d7a150518d7
KipkuruiYegon <kevinkipkurui1@gmail.com>
2023-11-23 19:49:51 +0300

    configured jinja templating

--- bookshopstore/static/js/google-map.js
+++ bookshopstore/static/js/google-map.js
@@ -5,8 +5,8 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
+    var myLatlng = new google.maps.LatLng(-1.286389, 36.821944);  // Coordinates for Kenyatta Avenue, Nairobi CBD
+ // 39.399872
     // -8.224454
     
     var mapOptions = {
@@ -43,7 +43,8 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
+//    var addresses = ['New York'];
+      var addresses = ['Kenyatta Avenue, Nairobi, Kenya'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
--- bookshopstore/views.py
+++ bookshopstore/views.py
@@ -6,22 +6,31 @@ from .models import Book
 
 def home(request):
     bookItem = Book.objects.all().order_by('?')
-    return render(request, 'home.html', {'bookItem':bookItem})
+    return render(request, 'home.html', {'bookItem':bookItem, 'navbar':'home'})
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'navbar':'about'})
 
 def news(request):
     return render(request, 'news.html')
 
 def bookcollection(request):
-    return render(request, 'bookcollection.html')
+    return render(request, 'bookcollection.html', {'navbar':'bookcollection'})
+
+def book_details(request):
+    return render(request, 'book_details.html', {'navbar':'book_details'})
+
+def blog_details(request):
+    return render(request, 'blog_details.html' )
 
 def cart(request):
-    return render(request, 'cart.html')
+    return render(request, 'cart.html', {'navbar':'cart'})
+
+def blog(request):
+    return render(request, 'blog.html', {'navbar':'blog'})
 
 def checkout(request):
-    return render(request, 'checkout.html')
+    return render(request, 'checkout.html', {'navbar':'checkout'})
 
 def login(request):
     return render(request, 'login.html')
@@ -31,7 +40,7 @@ def register(request):
 def thankyou(request):
     return render(request, 'thankyou.html')
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', {'navbar':'contact'})
 def errorpage(request):
     return render(request, '404page.html')
 
